5c880f8abb7c7d43e90643fee8d721c2445037ba
glassonionmovies <125755417+glassonionmovies@users.noreply.github.com>
2025-08-09 16:03:05 -0400

    Update random_api.py

--- random_api.py
+++ random_api.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def get_random_number():
     return jsonify({'random_number': random.randint(1, 100)})
 
+@app.route('/srand', methods=['GET'])
+def get_simple_random_number():
+    return str(random.randint(1, 100))
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
